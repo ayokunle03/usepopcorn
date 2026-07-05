07496a118dd5c7218bf5cb3a65ee964c48ab8c95
Ayokunle03 <ayokunleayodele03@gmail.com>
2026-07-05 23:59:42 +0100

    adding hover effect on the star rating component

--- src/StarRating.js
+++ src/StarRating.js
@@ -19,6 +19,7 @@ const textStyle = {
 
 export default function StarRating({maxRating = 5}) {
 const [rating, setRating] = useState(0);
+const [tempRating, setTempRating] = useState(0);
 
 function handleRating(rate) {
     setRating(rate);
@@ -29,12 +30,15 @@ return(
         <div style={starContainerStyle}>
             {Array.from({ length: maxRating }, (_, index) => (
                 <Star 
-                full={rating >= index + 1}
+                full={tempRating ? tempRating >= index + 1 : rating >= index + 1}
                 key={index}
-                onRate={() => handleRating(index + 1)} />
+                onRate={() => handleRating(index + 1)} 
+                onHoverIn={() => setTempRating(index + 1)}
+                onHoverOut={() => setTempRating(0)}
+                />
             ))}
         </div>
-        <p style={textStyle}>{rating || ""}</p>
+        <p style={textStyle}>{tempRating || rating || ""}</p>
         </div>
     )}
 
@@ -47,10 +51,15 @@ const starStyle = {
 }; 
 
 
-function Star({ onRate, full = false }) {
+function Star({ onRate, full = false , onHoverIn, onHoverOut}) {
 
 return(
-<span role="button" style={starStyle} onClick={onRate}>
+<span role="button" 
+style={starStyle} 
+onClick={onRate}
+onMouseEnter={onHoverIn}
+onMouseLeave={onHoverOut}
+>
            {full ? (
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
